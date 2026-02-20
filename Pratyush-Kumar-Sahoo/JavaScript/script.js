b1b978e1a410f202d01a5d2e04ead46f5c90cca6
Pratyush Sahoo <pratyush.sahoo@hotmail.com>
2026-02-20 18:31:09 +0530

    java_variables

---
+++ Pratyush-Kumar-Sahoo/JavaScript/script.js
@@ -0,0 +1,27 @@
+//var
+var a = "Hii";  // DYNAMIC TYPING !!
+console.log("Hello World")
+console.log(a)
+console.log(2+3)
+// //let and const
+// let b = 5;
+// console.log(b);
+// console.log(typeof true);
+
+const person = {name: "Pratyush"}
+person.name = "sahoo"
+console.log(person.name)
+
+{
+    let x = 10;
+    var y = 10;
+    console.log(x)
+    console.log(y)
+}
+// console.log(x) // ERROR
+console.log(y)
+
+
+
+console.log(xyz)
+let xyz ;
