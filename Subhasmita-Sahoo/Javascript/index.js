12b6a9806216bd8632c114f3f7f6bca139784110
Subhasmita Sahoo <subhasmita1606@gmail.com>
2026-02-21 18:16:16 +0530

    js fundamental

---
+++ Subhasmita-Sahoo/Javascript/index.js
@@ -0,0 +1,20 @@
+console.log("hello world")
+
+//const
+
+//const age=10;
+//age=60;
+
+//{
+//    const age=50
+//}
+//console.log(age)
+
+//if(true){
+//     const coll="ITER"
+//}
+//console.log(coll)
+
+//Hoisting
+console.log(a)
+var a=10
