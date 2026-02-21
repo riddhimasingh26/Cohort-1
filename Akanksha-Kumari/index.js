75b2ccb5ecfcad0163798769a0cd1a4679147e41
Akanksha Kumari <akankshakumariii02@gmail.com>
2026-02-21 18:21:20 +0530

    Fundamentals of Java Script

---
+++ Akanksha-Kumari/index.js
@@ -0,0 +1,14 @@
+// console.log("hello world")
+// console.log("second")
+
+
+{
+    let name="Akanksha"
+    console.log(name)
+    {
+        console.log(name)
+        // let name="Saswat"
+        console.log(name)
+    }
+}
+
