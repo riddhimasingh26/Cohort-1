989b908ffd3567017fe6fd55a63b45e9d7989c33
Arpita Mishra <arpitamishra30062005@gmail.com>
2026-02-21 22:24:20 +0530

    my first class of java script

---
+++ Arpita-Mishra/JavaScript/index.js
@@ -0,0 +1,52 @@
+// console.log("Hello World")
+// console.log(10-1)
+// console.log(true)
+
+// var x=10;
+// var x=10
+// let y=10
+// const z=10
+// var t="110"    //dynamic typing means data type specify krne ka need nahi h 
+// x=10
+// y=12       // x y me no issue
+// z=11      // z wale pe error aayega becoz const laga hua h z k aage const means contsant means ab usko change nhi kr skte
+
+
+// var x; //declaration
+// x=10 //initialisation
+
+// var x=10;
+// {
+// console.log(x)
+// }
+
+// {
+//     var x=10
+//     console.log(x)
+// }
+//  console.log(x)
+// var is global scoped and functional scoped
+
+
+// {   let y=20
+//     var x=10
+//     console.log(x)
+//     console.log(y)
+
+// }
+// console.log(x)
+// console.log(y)
+
+// let x=10
+// console.log(x)\
+
+// console.log(x)
+//let x=10     // error dega 
+
+// in case of let dead zone  mtlb if pehle x ko print kra re and baad me initialize kr re then wo print statement k upppr ka sab ko dead zone bolte h wo x ka dead zone h 
+
+// console.log(x)
+// var x=10;    // yaha erroe nai dega  bass undefined likh dega mtlb ki initialize nhi hua h 
+
+
+// by default value of var is undefined
